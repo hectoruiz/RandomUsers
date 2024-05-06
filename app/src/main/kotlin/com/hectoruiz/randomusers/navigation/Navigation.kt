8e5b643d3292f5afc83b6a3cf931425a868b3d10
hectoruiz <hectoruiz.89@gmail.com>
2024-05-06 20:25:53 +0200

    Changes in navigation compose graph definition and add transitions between the screens Add a few tests of compose navigation

--- app/src/main/kotlin/com/hectoruiz/randomusers/navigation/Navigation.kt
+++ app/src/main/kotlin/com/hectoruiz/randomusers/navigation/Navigation.kt
@@ -1,54 +1,119 @@
 package com.hectoruiz.randomusers.navigation
 
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideIn
+import androidx.compose.animation.slideOut
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.unit.IntOffset
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.navigation.NavHostController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
+import com.hectoruiz.domain.commons.Error
 import com.hectoruiz.ui.userdetail.UserDetailScreen
 import com.hectoruiz.ui.userdetail.UserDetailViewModel
-import com.hectoruiz.ui.userlist.ErrorState
 import com.hectoruiz.ui.userlist.UserListScreen
 import com.hectoruiz.ui.userlist.UserListViewModel
 
 @Composable
-fun NavGraph() {
-    val navController = rememberNavController()
-
+fun NavigationGraph(navController: NavHostController) {
     NavHost(navController = navController, startDestination = START_DESTINATION) {
-        composable(route = START_DESTINATION) {
+        composable(
+            route = START_DESTINATION,
+            enterTransition = {
+                slideIn(initialOffset = { IntOffset(300, 200) }) + fadeIn(
+                    tween(
+                        ANIMATION_LIST_LENGTH
+                    )
+                )
+            },
+            popEnterTransition = {
+                slideIn(initialOffset = { IntOffset(300, 200) }) + fadeIn(
+                    tween(
+                        ANIMATION_LIST_LENGTH
+                    )
+                )
+            },
+            exitTransition = {
+                slideOut(targetOffset = { IntOffset(300, 200) }) + fadeOut(
+                    tween(
+                        ANIMATION_LIST_LENGTH
+                    )
+                )
+            },
+            popExitTransition = {
+                slideOut(targetOffset = { IntOffset(300, 200) }) + fadeOut(
+                    tween(
+                        ANIMATION_LIST_LENGTH
+                    )
+                )
+            }
+        ) {
             val userListViewModel = hiltViewModel<UserListViewModel>()
-            val loading by userListViewModel.loading.collectAsStateWithLifecycle()
-            val users by userListViewModel.users.collectAsStateWithLifecycle()
-            val error by userListViewModel.error.collectAsStateWithLifecycle(ErrorState.NoError)
+            val uiState by userListViewModel.uiState.collectAsStateWithLifecycle()
+            val users by userListViewModel.users.collectAsStateWithLifecycle(emptyList())
+            val error by userListViewModel.error.collectAsStateWithLifecycle(Error.NoError)
 
             UserListScreen(
                 users = users,
-                loading = loading,
+                uiState = uiState,
                 error = error,
-                onClickMoreImages = { userListViewModel.fetchUsers() },
+                onUserSearch = { userListViewModel.searchUsers(it) },
+                onClickMoreUsers = { userListViewModel.getMoreUsers() },
                 onDeleteUser = { userListViewModel.deleteUser(it) },
                 navigateToDetail = {
                     navController.navigate("$FINAL_DESTINATION/$it")
-                }
+                },
             )
         }
         composable(
             route = "$FINAL_DESTINATION/{$PARAM}",
-            arguments = listOf(navArgument(PARAM) { type = NavType.StringType })
+            arguments = listOf(navArgument(PARAM) { type = NavType.StringType }),
+            enterTransition = {
+                slideIn(initialOffset = { IntOffset(300, 200) }) + fadeIn(
+                    tween(
+                        ANIMATION_DETAIL_LENGTH
+                    )
+                )
+            },
+            popEnterTransition = {
+                slideIn(initialOffset = { IntOffset(300, 200) }) + fadeIn(
+                    tween(
+                        ANIMATION_DETAIL_LENGTH
+                    )
+                )
+            },
+            exitTransition = {
+                slideOut(targetOffset = { IntOffset(300, 200) }) + fadeOut(
+                    tween(
+                        ANIMATION_DETAIL_LENGTH
+                    )
+                )
+            },
+            popExitTransition = {
+                slideOut(targetOffset = { IntOffset(300, 200) }) + fadeOut(
+                    tween(
+                        ANIMATION_DETAIL_LENGTH
+                    )
+                )
+            }
         ) {
             val userDetailViewModel = hiltViewModel<UserDetailViewModel>()
             val userDetailUiState by userDetailViewModel.userDetailUiState.collectAsStateWithLifecycle()
 
-            UserDetailScreen(userDetailUiState = userDetailUiState)
+            UserDetailScreen(userDetailUiState = userDetailUiState) { navController.popBackStack() }
         }
     }
 }
 
-private const val START_DESTINATION = "userList"
-private const val FINAL_DESTINATION = "userDetail"
-const val PARAM = "userId"
+const val START_DESTINATION = "userList"
+const val FINAL_DESTINATION = "userDetail"
+const val PARAM = "email"
+private const val ANIMATION_LIST_LENGTH = 1000
+private const val ANIMATION_DETAIL_LENGTH = 500
