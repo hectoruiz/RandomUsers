99936dd0b01574f4e0ad9a748a5f7565fdbf6ab2
hectoruiz <hectoruiz.89@gmail.com>
2024-05-06 20:21:18 +0200

    Changes at Hilt modules in instances creations Avoid migration of the Room DDBB

--- app/src/main/kotlin/com/hectoruiz/randomusers/di/NetworkModule.kt
+++ app/src/main/kotlin/com/hectoruiz/randomusers/di/NetworkModule.kt
@@ -1,15 +1,21 @@
 package com.hectoruiz.randomusers.di
 
 import com.hectoruiz.data.api.remote.ApiClient
+import com.hectoruiz.data.api.remote.ApiClient.Companion.TIMEOUT
 import com.hectoruiz.data.api.remote.ApiService
 import com.hectoruiz.data.datasources.remote.UserRemoteDataSourceImpl
 import com.hectoruiz.data.repositories.UserRemoteDataSource
+import com.hectoruiz.randomusers.BuildConfig
 import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.create
+import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 @Module
@@ -19,8 +25,29 @@ abstract class NetworkModule {
     companion object {
 
         @Provides
-        fun providerApiClient(): ApiClient {
-            return ApiClient()
+        fun providerLoggingInterceptor(): Interceptor {
+            return HttpLoggingInterceptor().apply {
+                setLevel(
+                    if (BuildConfig.DEBUG) {
+                        HttpLoggingInterceptor.Level.BODY
+                    } else {
+                        HttpLoggingInterceptor.Level.NONE
+                    }
+                )
+            }
+        }
+
+        @Provides
+        fun providerOkHttpClient(interceptor: Interceptor): OkHttpClient {
+            return OkHttpClient.Builder().connectTimeout(TIMEOUT, TimeUnit.SECONDS)
+                .addInterceptor(interceptor)
+                .readTimeout(TIMEOUT, TimeUnit.SECONDS).build()
+
+        }
+
+        @Provides
+        fun providerApiClient(okHttpClient: OkHttpClient): ApiClient {
+            return ApiClient(okHttpClient)
         }
 
         @Provides
